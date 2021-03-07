94dcacde0dd3f9d2ae43e5652257c3e201f4bb22
Isaac <isaacgeazy@gmail.com>
2021-03-07 13:29:35 -0300

    NomeCompleto

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenm;
+
+            Console.Write("Seu nome: ");
+            nome = Convert.ToString(Console.ReadLine());
+            Console.Write("Seu sobrenome: ");
+            sobrenm = Convert.ToString(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine($"Nome Completo: {nome} {sobrenm}");
+            Console.WriteLine($"Nome de Catálogo: {sobrenm.ToUpper()}, {nome}");
+     
+        }
+    }
+}
